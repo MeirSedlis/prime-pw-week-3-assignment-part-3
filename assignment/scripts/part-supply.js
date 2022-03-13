3e587bf78bfddee44b594e41da783eccfd661ac1
Meir Sedlis <meirsedlis@gmail.com>
2022-03-13 17:07:05 -0500

    assignment completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -34,7 +34,18 @@ supplyChanges.push(25);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for(value of supplyChanges){
+    if(value > 0){
+        console.log( 'Added', value, 'parts')
+    }  //code if positive
+    else if (value === 0){
+        console.log( 'No change.' )
+    }//code if 0
+    else {
+        console.log( 'Removed', Math.abs(value), 'parts' )
+    }
+    //removal code. The double negative was irking me so that Math.abs seemed like an elegants solution!
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
