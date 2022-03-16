e14c5b8a7cf3efabadeb427714cc3fa64a69986b
Meir Sedlis <meirsedlis@gmail.com>
2022-03-16 12:18:57 -0500

    corrected question four to match expected syntax

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -20,9 +20,9 @@ console.log('3. Second supplyChange is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:', supplyChanges[5]);
+let removedItem = supplyChanges.pop();
 supplyChanges.pop();
-
+console.log(removedItem, 'was added by mistake and has been removed');
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
